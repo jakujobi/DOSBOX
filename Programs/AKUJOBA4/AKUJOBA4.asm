871fb8e884dd4f1d2a8a4877e0997fcf63a43777
jakujobi <akujobi365@gmail.com>
2023-10-21 22:31:38 -0500

    Added more documentation.

--- Programs/AKUJOBA4/AKUJOBA4.asm
+++ Programs/AKUJOBA4/AKUJOBA4.asm
@@ -31,10 +31,9 @@ output_msg db "Fahrenheit: ", '$',
 extrn PutDec:near
 extrn GetDec:near
 
-JAKUJ PROC
-
+JAKUJ PROC  ; Start of main procedure
     _Begin
-    _PutStr input_msg
+    _PutStr input_msg   ; Write the text in input_msg
 
     ; Get Celsius temperature from user
     call GetDec ; Get the celsius temperature and store in ax
@@ -68,3 +67,19 @@ _Exit 0 ; Exit the program with exit code 0
 JAKUJ    endp    ; End of main procedure
 
 End JAKUJ    ; End of program
+
+
+;This program is designed to convert a temperature in Celsius to Fahrenheit.
+;It prompts the user to enter a temperature in Celsius, reads the input,
+; converts it to Fahrenheit using the formula Fahrenheit = (Celsius * 9/5) + 32, and displays the result.
+
+;The program uses the following registers:
+;   AX: used to store the Celsius temperature and the result of the multiplication and division operations
+;   BX: used to store the value 9 for multiplication with the Celsius temperature
+;   BL: used to store the value 5 for division with the result of the multiplication operation
+;   CX: used to store the Celsius temperature and a copy of the result of the multiplication operation
+;   INT 21h: used to display the output message
+
+;The program also uses two external procedures:
+;   PutDec: used to display the result of the Fahrenheit conversion
+;   GetDec: used to read the Celsius temperature input from the user

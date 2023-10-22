2de705ca1d2215d938e1f715b3ea4423269b70e8
jakujobi <akujobi365@gmail.com>
2023-10-21 19:38:58 -0500

    started project 4.

---
+++ Programs/AKUJOBA4/AKUJOBA4.asm
@@ -0,0 +1,48 @@
+; Name: Celsius to Fahrenheit Conversion
+; Class: CSC 314
+; Assign: Assignment 4
+; Due: 21st October 2023
+
+; Description:
+; This program converts a temperature from Celsius to Fahrenheit.
+; It uses the formula: Fahrenheit = (Celsius * 9/5) + 32
+
+;;To Run
+;; Open DosBox
+;; Navigate to:
+;;     c:\path\to\asm\file
+;; Type:
+;;      nasm -f obj filename.asm
+;; Type and enter:
+;;     link filename.obj,,,util
+;; Type and enter:
+;;     filename.exe
+
+include pcmac.inc  ; Include pcmac.inc file
+.model small  ; Set memory model to small
+.586  ; Target the Intel 586 processor
+.stack 100h  ; Set stack size to 256 bytes (100h in hexadecimal)
+
+.data  ; Start of data segment
+input_msg db "Enter temperature in Celsius: ", '$'
+output_msg db "Fahrenheit: ", '$', 
+
+.CODE
+extrn PutDec:near
+extrn GetDec:near
+
+JAKUJ PROC
+
+    _Begin
+    _PutStr input_msg
+
+
+
+    ; Exit
+    _Exit 0
+
+;;_________________________________________________________
+_Exit 0 ; Exit the program with exit code 0
+JAKUJ    endp    ; End of main procedure
+
+End JAKUJ    ; End of program

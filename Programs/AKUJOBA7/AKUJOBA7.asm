89ae19b4083f963d0cf9de4b0fd31bb3d3e2cf66
jakujobi <akujobi365@gmail.com>
2023-12-11 19:05:11 -0600

    Update to include variables, arrays, and labels.

--- Programs/AKUJOBA7/AKUJOBA7.asm
+++ Programs/AKUJOBA7/AKUJOBA7.asm
@@ -18,26 +18,37 @@
 ;; Type and enter:
 ;;     filename.exe
 
-include pcmac.inc  ; Include pcmac.inc file
-.model small  ; Set memory model to small
-.586  ; Target the Intel 586 processor
-.stack 100h  ; Set stack size to 256 bytes (100h in hexadecimal)
+include pcmac.inc           ; Include pcmac.inc file
+.model small                ; Set memory model to small
+.586                        ; Target the Intel 586 processor
+.stack 100h                 ; Set stack size to 256 bytes (100h in hexadecimal)
 
-.data  ; Start of data segment
-;; msg db "Hello World!", 13. 10 "$"  ; Define a string
-;; Define variables here
+.data                       ; Start of data segment
+NameArray db 80 dup(?)      ; Array to store the name, max 80 characters
+length dw 0                 ; Variable to store the length of the name
 
+;
+enterKey db 13              ; ASCII code for Enter key
+comma db 44                 ; ASCII value for comma
+theSpace db 32                 ; ASCII value for space
+
+AskForName db "Hi there!", 13, 10 "Type your name at the arrow below in the form of [FirstName MiddleName LastName]", 13, 10 "->", '$'
+    
 
 ;;_________________________________________________________
 .code  ; Start of code segment
-main    proc
-_Begin
-;_PutStr msg  ; Print the string (msg) to the screen [This overwrites ax]
-;; Type your code here
+extrn GetDec:near
+extrn PutDec:near
+
+;;This ReadUsersName procedure 
+ReadUsersName proc
+
+ReadUsersName endp
 
 
-;;_________________________________________________________
-_Exit 0 ; Exit the program with exit code 0
-main    endp    ; End of main procedure
+JAKUJ    proc
+
+JAKUJ    endp    ; End of main procedure
 
 End main
+

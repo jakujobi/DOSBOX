e37a5a4b5eb10a8ddbeb21e5cea94031fa584a8c
jakujobi <akujobi365@gmail.com>
2023-12-11 20:30:14 -0600

    Add welcome message and error handling for empty names

--- Programs/AKUJOBA7/AKUJOBA7.asm
+++ Programs/AKUJOBA7/AKUJOBA7.asm
@@ -25,62 +25,106 @@ include pcmac.inc           ; Include pcmac.inc file
 
 .data                       ; Start of data segment
 NameArray db 80 dup(?)      ; Array to store the name, max 80 characters
-length dw 0                 ; Variable to store the length of the name
+NameLength dw ?                 ; Variable to store the length of the name
 
 ;Characters
 enterKey db 13              ; ASCII code for Enter key
 comma db 44                 ; ASCII value for comma
-theSpace db 32                 ; ASCII value for space
-
-;Message
-AskForName db "Hi there!", 13, 10 "Type your name at the arrow below in the form of [FirstName MiddleName LastName]", 13, 10, "Then press enter", 13, 10, "->", '$'
-    
+theSpace db 32                 ; ASCII value for space    
 
 ;;_________________________________________________________
 .code  ; Start of code segment
-extrn GetDec:near
-extrn PutDec:near
+;extrn GetDec:near
+;extrn PutDec:near
 
-;;This ReadUsersName procedure 
+WelcomeMessage proc
+    _PutStr 13, 10, "Hi there!", 13, 10, "Type your name at the arrow (--->)at the bottom of this message", 13, 10, "Then press enter"
+    _PutStr 13, 10, "* It should be in the form of [FirstName MiddleName LastName]"
+    _PutStr 13, 10, "* No empty names please, and no more than 80 characters"
+    _PutStr 13, 10, "After that, press enter"
+    _PutStr 13, 10, "Thank You!", 13, 10, "--->", 13, 10, '$'
+    ret
+WelcomeMessage endp
+
+;;ReadUsersName procedure gets the name from the user
 ReadUsersName proc
-    _PutStr AskForName
+    call WelcomeMessage
     xor cx, cx                  ; we'll set cx to 0 and use as an index for the array (c for counter)
+    lea di, NameArray           ; Load address of NameArray into di register (d for data)
+
+    xor ax,ax                   ; set ax to 0
 readingLoop:
     _GetCh                      ; read a character
-    cmp al, enterKey            ; check if enter key is pressed
+    cmp al, enterKey            ; check if enter key is pressed (register a has the character)
     je endReading               ; if yes, end reading
 
-    mov [dx], al                ; store the character in the array
-    inc dx                      ; increment the pointer
+    mov [di], al                ; store the character in the array
+    add di, 1                   ; increment the pointer
 
     inc cx                      ; increment the counter
     cmp cx, 80                  ; check if the array is full
-    jge endReading              ; if yes, end reading
+    jge toolong                 ; if yes, end reading
 
     jmp readingLoop             ; continue reading
 
+callTooLong:
+    call toolong                ; call toolong procedure to print an error message
+
 endReading:
+    mov NameLength , cx            ; store the length of the name into length variable
+    ret
 
 ReadUsersName endp
 
-; Procedure to read a name
-ReadName PROC
-    mov bx, 0                   ; Initialize index to 0
-readLoop:
-    _GetCh                      ; Read a character
-    cmp al, enterKey            ; Compare with Enter key
-    je endRead                  ; If Enter key, end reading
-    mov name[bx], al            ; Store character in array
-    inc bx                      ; Increment index
-    jmp readLoop                ; Repeat loop
-endRead:
-    mov nameLength, bx          ; Store name length
+
+;;toolong procedure prints an error message if the name is too long
+toolong proc
+    pushad                      ; save all registers
+    _PutStr 13, 10, "Array limit of 80 characters reached!", 13, 10, '$'
+    popad                       ; restore all registers
     ret
-ReadName ENDP
+toolong endp
 
+
+;;PrintUsersName procedure prints the name in the form of [LastName, FirstName MiddleName]
+PrintUsersName proc
+    xor cx, cx                  ; we'll set cx to 0 and use as an index for the array (c for counter)
+    mov cx, NameLength              ; load the length of the name into cx
+    dec cx                      ; decrement cx by 1
+
+    lea di, NameArray           ; Load address of NameArray into di register (d for data)
+
+PrintUsersName endp
+
+;;NameIsEmpty procedure prints an error message if the name is empty
+nameIsEmpty proc
+    pushad                      ; save all registers
+    _PutStr 13, 10, "Huh!, your name is _...empty? Thats not a name!", 13, 10, "Let's try again!", 13, 10, '$'
+    popad                       ; restore all registers
+    ret
+nameIsEmpty endp
+
+
+;;JAKUJ procedure is the main procedure
 JAKUJ    proc
+    _Begin
+    
+programstart:
+    call ReadUsersName
 
-JAKUJ    endp    ; End of main procedure
+    ;check if name is empty
+    mov bx, NameLength ;
+    cmp bx, 0
+    jg Notempty
 
-End main
+    call nameIsEmpty
+    jmp programstart
+
+Notempty:
+    call PrintUsersName
+    _Exit 0
+
+JAKUJ    endp    ; End of main procedure called JAKUJ
+
+End JAKUJ
 
